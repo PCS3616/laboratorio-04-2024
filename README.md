81b3833b2a2a40462841818cf9ed277ee64d60f3
Joao-Pedro-Cabral <miranda.jp@usp.br>
2023-12-27 20:15:59 -0300

    Versao 2024

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# PCS3616 - Laboratório 5 - MVN 2
+# PCS3616 - Laboratório 4 - MVN 2
 
 Nesta aula vamos continuar o trabalho da semana passada, hoje você deve
 criar mais alguns programas em linguagem de máquina de MVN.
 
-## 1. `triangulos.mvn`
-Escrever um programa que determina, para três valores a, b e c se
-estes valores: não formam um triângulo, formam um triângulo
-retângulo, triângulo acutângulo ou triângulo obtusângulo (valores 0,
-1, 2, 3, respectivamente). Usar as posições 0x010, 0x012, 0x014 para
-os valores a, b e c e a posição 0x016 para o resultado.
-
+## 1. `subtracao.mvn`
+Escrever um programa que executa a subtração
+de dois inteiros *em uma sub-rotina*. O programa principal
+armazena os inteiros nas posições 0x010 (variável x) e 0x012 (variável
+y) e chama a sub-rotina, que deve executar a operação x-y e armazenar o
+resultado na posição de memória 0x014.
 
 ## 2. `op-mnem.mvn`
 Desenvolva duas sub-rotinas (`OP2MNEM` e `MNEM2OP`), cujas finalidades são:
@@ -45,34 +44,3 @@ Exemplo de operação:
 -   Dado o mnemônico JZ em (MNEM, MNEM+1), ou seja, dado o par de bytes
     (0x4A, 0x5A), a sub-rotina MNEM2OP retornará em (OPCODE, OPCODE+1) o
     par de bytes (00, 01).
-
-
-## 3. quadrados-perfeitos.mvn
-Desenvolva um programa para a MVN que calcule e tabele, a partir da
-posição de memória 0x100, os quadrados perfeitos dos 64 primeiros
-números naturais (0x0, 0x1, 0x2, ..., 0x3F). O cálculo deverá ser
-realizado aplicando a seguinte interessante propriedade:
-
-$$n^2=\sum_{i=0}^{n-1}(2i+1)$$
-
-Cada quadrado calculado deverá ser armazenado como um inteiro de dois
-bytes, iniciando na posição de memória 0x100, ou seja, ao final da
-execução, os valores das posições de memória serão:
-
- |Posição (hexa)         |Valor (hexa)|Valor (decimal)|N                 |
- |-----------------------|------------|---------------|------------------|
- |100                    |0000        |0              |0                 |
- |102                    |0001        |1              |1                 |
- |104                    |0004        |4              |2                 |
- |...                    |...         |               |...               |
- |1??                    |0???        |3969           |63                |
-
-OBS.: caso você encontre problemas em algum exercício devido ao evitador
-de loops infinitos, você pode mudar o valor do número máximo de passos
-acionando a flag "-s \[valor\]" ou "\--max_step \[valor\]", trocando
-"\[valor\]" pelo limite de passos que você queira definir, ao chamar o
-módulo "mvnMonitor.py".
-
-\*:mostração intuitiva da fórmula
-
-![](./media/image1.png)
--- autogradle/main_test.py
+++ autogradle/main_test.py
@@ -5,10 +5,11 @@ import tempfile
 
 submission_path = Path("./submission")
 
-def limpa(string):
-    res=string.split(" ")
-    res=list(filter(None, res))
-    return int(res[1]+res[2],16)
+def twos_complement(hexstr,bits):
+    value = int(hexstr,16)
+    if value & (1 << (bits-1)):
+        value -= 1 << bits
+    return value
 
 def run_mvn(input_text):
     # I hate the current MVN
@@ -26,153 +27,42 @@ def run_mvn(input_text):
     )
     return p.stdout
 
-def test_1_1():
-    filecode = submission_path / "triangulos.mvn"
+def test_1():
+    filecode = submission_path / "ex2-subtracao.mvn"
     assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
+    
+    with open(filecode, mode='r') as f:
+        code = f.read().upper()
 
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0001\n",
-        "0012	0002\n",
-        "0014	0003\n",
-        ";0016	0000\n",
-    ])
-    input_file.flush()
+        assert len(re.findall(r"A[\dA-F]{3}", code)) > 0, \
+            "O seu código deve conter uma chamada de subrotina"
 
-    output_file = tempfile.NamedTemporaryFile(mode='r')
+        assert len(re.findall(r"B[\dA-F]{3}", code)) > 0, \
+            "O seu código deve conter uma subrotina"
 
     inputs = [
         f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
+         "r",
+         "",
+         "n",
+         "",
+         "m 0010 0015",
+         "x",
+         "",
     ]
 
-    out = run_mvn('\n'.join(inputs))
+    output = run_mvn('\n'.join(inputs))
 
-    mvn_output = limpa(output_file.read())
+    saida=output.split("\n")[-6:-3]
+    print(saida)
 
-    assert mvn_output == 0, \
-            f"Seu código não está correto"
+    nums=saida[-1].split("  ")
+    x=twos_complement(nums[1]+nums[2], 16)
+    y=twos_complement(nums[3]+nums[4], 16)
+    r=twos_complement(nums[5]+nums[6], 16)
 
-def test_1_2():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0003\n",
-        "0012	0004\n",
-        "0014	0005\n",
-        ";0016	0001\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 1, \
-            f"Seu código não está correto"
-
-def test_1_3():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0003\n",
-        "0012	0004\n",
-        "0014	0004\n",
-        ";0016	0002\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 2, \
-            f"Seu código não está correto"
-
-def test_1_4():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0003\n",
-        "0012	0004\n",
-        "0014	0006\n",
-        ";0016	0003\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 3, \
-            f"Seu código não está correto"
+    assert x-y == r, \
+        f"Seu código não está correto\nConfira seu envio."
 
 def test_2():
     filecode = submission_path / "op-mnem.mvn"
@@ -239,39 +129,4 @@ def test_2():
     print(mvn_output)
 
     assert mvn_output == "00  01  00  01", \
-            f"Seu código não está correto"
-
-def test_3():
-    filecode = submission_path / "quadrados-perfeitos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0100 017f {output_file.name}",
-        "",
-        "x",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = output_file.read()
-    expected_output = """
-0100:  00  00  00  01  00  04  00  09  00  10  00  19  00  24  00  31  
-0110:  00  40  00  51  00  64  00  79  00  90  00  a9  00  c4  00  e1  
-0120:  01  00  01  21  01  44  01  69  01  90  01  b9  01  e4  02  11  
-0130:  02  40  02  71  02  a4  02  d9  03  10  03  49  03  84  03  c1  
-0140:  04  00  04  41  04  84  04  c9  05  10  05  59  05  a4  05  f1  
-0150:  06  40  06  91  06  e4  07  39  07  90  07  e9  08  44  08  a1  
-0160:  09  00  09  61  09  c4  0a  29  0a  90  0a  f9  0b  64  0b  d1  
-0170:  0c  40  0c  b1  0d  24  0d  99  0e  10  0e  89  0f  04  0f  81  
-Final do dump.""".strip()
-
-    assert mvn_output == expected_output, \
             f"Seu código não está correto"
